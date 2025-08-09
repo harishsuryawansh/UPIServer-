fb56fd3be5490ac5ef39f0d26d1a2f6a99874506
harish suryawanshi <141421801+harishsuryawansh@users.noreply.github.com>
2025-08-09 19:28:18 +0530

    here is new idea from developer harsh side ::

--- src/Main/UpiCalc.java
+++ src/Main/UpiCalc.java
@@ -1,6 +1,19 @@
 package Main;
 
+import java.util.Scanner;
+
+
+
 public class UpiCalc {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("enter a two number ");
+		int a = in.nextInt();
+		int b = in.nextInt();
+		int c = a+b;
+		System.out.println("the addition of your UPI payment " + c);
+		
+	}
 	
 
 }
